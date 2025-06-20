66e834869032325792a9cfb2174a3a5a39510ff8
Ezra Heinberg <ezra4@unc.edu>
2025-06-20 14:36:50 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import shutil
+import os
+
+app = FastAPI()
+
+# middleware
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"]
+                   )
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=False)
+
+@app.post("/upload")
+async def upload_audio(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file.filename, "path": file_path}
